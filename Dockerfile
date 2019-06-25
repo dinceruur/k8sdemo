3d48c41dead4e98d43d165753c3f0cf2e849048f
Ugur Dincer <dinceruur@gmail.com>
2019-06-26 01:47:07 +0300

    #3

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM golang:1.11-alpine as build
 
 WORKDIR /src/
 COPY main.go go.* /src/
+RUN apk add --no-cache git
+RUN CGO_ENABLED=0 go get gopkg.in/yaml.v2
 RUN CGO_ENABLED=0 go build -o /bin/demo
 
 FROM scratch
 COPY --from=build /bin/demo /bin/demo
 ENTRYPOINT ["/bin/demo"]
-
--- main.go
+++ main.go
@@ -1,29 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main(){
+const cfgPath = "/config/demo.yaml"
+
+var config struct {
+	Message string
+}
+
+func init() {
+	cfgData, err := ioutil.ReadFile(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = yaml.Unmarshal(cfgData, &config); err != nil {
+		log.Fatalf("failed to parse config file %q: %v", cfgPath, err)
+	}
+}
+
+func main() {
 	http.HandleFunc("/", sayHello)
 	http.HandleFunc("/ping", ping)
-	if err := http.ListenAndServe(":8080",nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func sayHello(w http.ResponseWriter, _ *http.Request){
-	_, err := w.Write([]byte("Hello"))
-	if err != nil{
+func sayHello(w http.ResponseWriter, _ *http.Request) {
+	message := fmt.Sprintf("Hello, %v", config.Message)
+	_, err := w.Write([]byte(message))
+	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func ping(w http.ResponseWriter, _ *http.Request){
+func ping(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, err := w.Write([]byte("Pong"))
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 }
